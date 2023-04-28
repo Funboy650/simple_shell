5cf4860683917c0b5b3c6705dcc28ace5b420484
Obiosaji <obiosajiqueen@gmail.com>
2023-04-28 08:23:40 -0700

    Simple_Shell_Project

--- errors.c
+++ errors.c
@@ -21,46 +21,46 @@ void _eputs(char *str)
 
 /**
  * _eputchar - Function that writes the character c to stderr
- * @c: The character to print
+ * @a: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _eputchar(char c)
+int _eputchar(char a)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (a == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
 		write(2, buf, i);
 		i = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (a != BUF_FLUSH)
+		buf[i++] = a;
 	return (1);
 }
 
 /**
- * _putfd -Function that writes the character c to given fd
- * @c: The character to print
+ * _putfd -Function that writes the character a to given fd
+ * @a: The character to print
  * @fd: The filedescriptor to write to
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putfd(char c, int fd)
+int _putfd(char a, int fd)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (a == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
 		write(fd, buf, i);
 		i = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (a != BUF_FLUSH)
+		buf[i++] = a;
 	return (1);
 }
 
@@ -83,4 +83,3 @@ int _putsfd(char *str, int fd)
 	}
 	return (i);
 }
-
