b519dd3e5534a9144411468ab7a11d0831d82492
Obiosaji <obiosajiqueen@gmail.com>
2023-04-28 08:33:27 -0700

    Simple_Shell_Project

--- exits.c
+++ exits.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
+ **_strncpy - Function that copies a string
  *@dest: the destination string will be copied to
  *@src: the string source
  *@n: the amount of characters to be copied
@@ -9,29 +9,29 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int a, b;
 	char *s = dest;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
+	a = 0;
+	while (src[a] != '\0' && a < n - 1)
 	{
-		dest[i] = src[i];
-		i++;
+		dest[a] = src[a];
+		a++;
 	}
-	if (i < n)
+	if (a < n)
 	{
-		j = i;
-		while (j < n)
+		b = a;
+		while (b < n)
 		{
-			dest[j] = '\0';
-			j++;
+			dest[b] = '\0';
+			b++;
 		}
 	}
 	return (s);
 }
 
 /**
- **_strncat - concatenates two strings
+ **_strncat - Function that concatenates two strings
  *@dest: the first string
  *@src: the second string
  *@n: the amount of bytes to be maximally used
@@ -39,34 +39,34 @@ char *_strncpy(char *dest, char *src, int n)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int a, b;
 	char *s = dest;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
+	a = 0;
+	b = 0;
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0' && b < n)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[a] = src[b];
+		a++;
+		b++;
 	}
-	if (j < n)
-		dest[i] = '\0';
+	if (b < n)
+		dest[a] = '\0';
 	return (s);
 }
 
 /**
- **_strchr - locates a character in a string
+ **_strchr - Function that locates a character in a string
  *@s: the string to be parsed
- *@c: the character to look for
+ *@g: the character to look for
  *Return: (s) a pointer to the memory area s
  */
-char *_strchr(char *s, char c)
+char *_strchr(char *s, char g)
 {
 	do {
-		if (*s == c)
+		if (*s == g)
 			return (s);
 	} while (*s++ != '\0');
 
